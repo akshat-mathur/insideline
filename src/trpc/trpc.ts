019a91ab1c2bf72faf5de01dcc37d58c8eb9aede
askshat-mathur <askshat0318.mathur@gmail.com>
2024-04-18 21:57:56 +0530

    middleware change

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -2,9 +2,9 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { TRPCError, initTRPC } from "@trpc/server";
 
 const t = initTRPC.create();
-const middleware = t.middleware;
 
-const isAuth = middleware(async (opts) => {
+
+const isAuth = t.middleware(async (opts) => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
